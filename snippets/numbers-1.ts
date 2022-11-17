0e20de153da53a045a53721eaafe12551e03ec5d
jovi <kwong.jovi@gmail.com>
2022-11-17 20:59:55 +0000

    add numbers-1.ts

---
+++ snippets/numbers-1.ts
@@ -0,0 +1,7 @@
+export const addTwoNumbers1 = (a: number, b: number) => {
+  return a + b;
+};
+
+export const addTwoNumbers2 = (params: { first: number; second: number }) => {
+  return params.first + params.second;
+};
